61ebd798e73a6f60f396a3e982c6f00b4daccd2b
Des-carte <nkrax2002@gmail.com>
2024-09-21 11:22:26 -0300

    parser

--- parser.py
+++ parser.py
@@ -4,3 +4,21 @@
 # - Retornar uma lista de tuplas, em que cada tupla representa as operações de uma transação e a posição i da lista
 #   se refere a transação i. (Essa estrutura pode ser parâmetro de uma função)
 
+import re
+
+def separate_operations(scheduler):
+    pattern = r'([rwc])(\d+)\((\w+)\)|([c])(\d+)'
+    operations = re.findall(pattern, scheduler)
+    
+    results = []
+    for op in operations:
+        if op[0]:
+            type = op[0]
+            transaction = op[1]
+            resource = op[2]
+            results.append((type, transaction, resource))
+        elif op[3]:
+            type = op[3]
+            transaction = op[4]
+            results.append((type, transaction, None))
+    return results
