021e57f771e32462022994e2886140a94aa6396f
Des-carte <nkrax2002@gmail.com>
2024-09-21 13:36:57 -0300

    get_write_lock & get_read_lock

--- lock_manager.py
+++ lock_manager.py
@@ -3,17 +3,30 @@ from enum import Enum
 
 # A classe deve lançar uma exceção sempre que ocorrer um deadlock
 class LockManager:
-    waits_for_graph = nx.DiGraph()
     # uma estrutura que dado um objeto(obj - x, y, z, ...), mostre seus bloqueios e a transação(txn) correspondente
     
     def __init__(self):
-        return
+        waits_for_graph = nx.DiGraph()
+        self.locks = {}
 
-    def get_write_lock(txn, obj):
-        return
+    def get_write_lock(self, txn, obj):
+        if obj in self.locks:
+            if self.locks[obj] != txn:
+                self.waits_for_graph.add_edge(txn, self.locks[obj])
+                if self.detect_deadlock():
+                    raise Exception
+                return False
+        self.locks[obj] = txn
+        return True
     
-    def get_read_lock(txn, obj):
-        return
+    def get_read_lock(self, txn, obj):
+        if obj in self.locks and self.locks[obj] != txn:
+            self.waits_for_graph.add_edge(txn, self.locks[obj])
+            if self.detect_deadlock():
+                raise Exception
+            return False
+        self.locks[obj] = txn
+        return True
     
-    def get_certify_lock(txn, obj);
+    def get_certify_lock(self, txn, obj);
         return
