48a62498c18d3e74017d21915096bf16403e39bc
Wanderson Soares <wandersonsoares841@gmail.com>
2024-09-21 20:51:59 -0300

    add deadlock exception

--- lock_manager.py
+++ lock_manager.py
@@ -1,6 +1,13 @@
 import networkx as nx
 from enum import Enum
 
+class DeadlockException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+    def __str__(self):
+        return f"Deadlock found when {self.args[0]}"
+
 # A classe deve lançar uma exceção sempre que ocorrer um deadlock
 class LockManager:
     # uma estrutura que dado um objeto(obj - x, y, z, ...), mostre seus bloqueios e a transação(txn) correspondente
@@ -14,7 +21,7 @@ class LockManager:
             if self.locks[obj] != txn:
                 self.waits_for_graph.add_edge(txn, self.locks[obj])
                 if self.detect_deadlock():
-                    raise Exception
+                    raise DeadlockException("trying to get lock from ...")
                 return False
         self.locks[obj] = txn
         return True
@@ -23,10 +30,10 @@ class LockManager:
         if obj in self.locks and self.locks[obj] != txn:
             self.waits_for_graph.add_edge(txn, self.locks[obj])
             if self.detect_deadlock():
-                raise Exception
+                raise DeadlockException("trying to get lock from ...")
             return False
         self.locks[obj] = txn
         return True
     
-    def get_certify_lock(self, txn, obj);
+    def update_to_certify_lock(self, txn, obj):
         return
