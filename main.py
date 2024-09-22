b806a3aa19dfddc86f2a74b55f58dee42526cef1
Wanderson Soares <wandersonsoares841@gmail.com>
2024-09-22 10:40:14 -0300

    io tty

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from scheduler import Scheduler
 import lock_manager as lm
+from os import isatty
+from sys import stdin
 
 raw_input = input('S = ')
+
+if not isatty(stdin.fileno()):
+    print(raw_input)
 
 # parser raw_input
 
@@ -15,11 +20,12 @@ for request in requests:
     try:
         operation = request[0]
         txn = request[1]
-        obj = request[3]
 
         if operation == 'r':    # read
+            obj = request[3]
             scheduler.read(txn, obj)
         elif operation == 'w':  # write
+            obj = request[3]
             scheduler.write(txn, obj)
         else:                   # commit
             scheduler.commit(txn)
